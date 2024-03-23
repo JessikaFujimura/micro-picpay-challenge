adf18e9458565f5bb25d8b08ac2ff619a1cd7762
jessika <jessikafujimura@gmail.com>
2024-03-23 18:22:35 -0300

    files: iniciate the project

---
+++ src/main/java/br/com/micropicpaychallenge/MicroPicpayChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.micropicpaychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPicpayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPicpayChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/micropicpaychallenge/MicroPicpayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.micropicpaychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPicpayChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
